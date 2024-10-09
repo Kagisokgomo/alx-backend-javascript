b4c4f6294be634959236694caae318a5b0a684d5
Kagisokgomo <kagisomalobela29@gmail.com>
2024-10-09 13:16:27 +0000

    0x02. ES6 classes

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,20 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this
